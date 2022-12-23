7b57203fa82763f963cfa97b303231c168d5fab7
Farul Ivan <farulivan@gmail.com>
2022-12-23 08:17:29 +0700

    adding input box, button and screenBgColor

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,28 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const PrimaryButton = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{children}</Text>
     </View>
   );
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  container: {
+    width: 120,
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F2CB05',
+    paddingVertical: 6,
+    paddingHorizontal: 18,
+    borderRadius: 40,
+  },
+  text: {
+    color: '#405B66',
+    fontWeight: 'bold',
+  },
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,9 +4,21 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput
+        style={styles.inputText}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoComplete='off'
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.button}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.button}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -15,8 +27,38 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
+    marginHorizontal: 24,
     padding: 16,
+    backgroundColor: '#405B66',
+    borderRadius: 8,
+    // android
+    elevation: 4,
+    // ios & android
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6, 
+    shadowOpacity: 0.25,
+  },
+  inputText: {
+    marginVertical: 8,
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 32,
+    color: '#F2CB05',
+    borderBottomColor: '#F2CB05',
+    borderBottomWidth: 2,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 18,
+  },
+  button: {
+    flex: 1,
+    alignItems: 'center',
   },
 });
